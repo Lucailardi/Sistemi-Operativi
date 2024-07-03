b8fb6a3f7772d30e2b2d5b7767c6e0d656ebaf81
abramo1 <abramo.azer01@universitadipavia.it>
2024-07-03 13:01:47 +0200

    bhooo

---
+++ SO/src/iii/unipv/soproject/Ordine.java
@@ -0,0 +1,13 @@
+package iii.unipv.soproject;
+
+public class Ordine {
+    private String descrizione;
+
+    public Ordine(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+}
